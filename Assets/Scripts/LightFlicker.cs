e0601f6fd749b16ab1ef0af547aa3d096bb57b27
Joseph Quested <josephquested@gmail.com>
2017-04-12 20:49:01 +1200

    add dynamic sprite lighting

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+	Light _light;
+
+	public bool flicker;
+
+	public float minFlickerIntensity;
+  public float maxFlickerIntensity;
+  public float flickerSpeed;
+
+  float randomizer = 0;
+
+	void Start ()
+	{
+		_light = GetComponent<Light>();
+		StartCoroutine(FlickerRoutine());
+	}
+
+	IEnumerator FlickerRoutine ()
+	{
+		while (flicker)
+		{
+			print("running");
+			if (randomizer == 0)
+			{
+				_light.intensity = Random.Range(minFlickerIntensity, maxFlickerIntensity);
+			}
+			else
+			{
+				_light.intensity = Random.Range(minFlickerIntensity, maxFlickerIntensity);
+			}
+			randomizer = Random.Range(0, 1.1f);
+			yield return new WaitForSeconds(flickerSpeed);
+		}
+	}
+}
