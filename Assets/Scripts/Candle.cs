88e0e9adde2030166153de4eb5f4b1ef94696a45
josephquested <josephquested@gmail.com>
2017-04-14 21:29:59 +1200

    update candle sprite

---
+++ Assets/Scripts/Candle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candle : Interactable {
+
+	public Light light1;
+	public Light light2;
+
+	public override void Fire ()
+	{
+		GetComponent<Animator>().SetTrigger("Snuff");
+		light1.enabled = false;
+		light2.enabled = false;
+	}
+}
---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+	public bool hot;
+
+	public virtual void Fire ()
+	{
+		// override
+	}
+
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		if (collider.GetComponent<Interact>() != null)
+		{
+			hot = true;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D collider)
+	{
+		if (collider.GetComponent<Interact>() != null)
+		{
+			hot = false;
+		}
+	}
+}
